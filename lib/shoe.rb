201063e176c2b4dba3402ca66c3676959ac80487
Trisha Garrett <trisha.garrett@gmail.com>
2017-11-28 01:00:34 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,13 @@
 # Make your shoe class here!
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  
+
   def initialize (brand)
     @brand = brand
   end
 
   def brand
     @brand
-  end  
+  end
 
 end
